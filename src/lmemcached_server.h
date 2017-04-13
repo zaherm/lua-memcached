6261054cd9f72198bd5882b42b396c56bb8e62ff
zaherm <zaherm@gmail.com>
2017-04-13 18:58:47 +0000

    Added server functions to header file

--- src/lmemcached_server.h
+++ src/lmemcached_server.h
@@ -9,10 +9,18 @@ typedef struct {
   const memcached_instance_st *ptr;
 } lmemcached_server;
 
+lmemcached_server *lmemcached_server_check(lua_State *L, int index);
+
 LUALIB_API int lmemcached_server_create(lua_State *L);
 LUALIB_API int lmemcached_server_free(lua_State *L);
 LUALIB_API int lmemcached_server_index(lua_State *L);
-
+LUALIB_API int lmemcached_server_response_count(lua_State *L);
+LUALIB_API int lmemcached_server_port(lua_State *L);
+LUALIB_API int lmemcached_server_srcport(lua_State *L);
+LUALIB_API int lmemcached_server_type(lua_State *L);
+LUALIB_API int lmemcached_server_major_version(lua_State *L);
+LUALIB_API int lmemcached_server_minor_version(lua_State *L);
+LUALIB_API int lmemcached_server_micro_version(lua_State *L);
 LUALIB_API void lmemcached_server_open(lua_State *L);
 #endif
 
